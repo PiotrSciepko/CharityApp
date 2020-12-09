80971e1f11ffda8fd80b0d0c508483a86505b249
PiotrSciepko <piotrek.priv@gmail.com>
2020-12-09 18:22:51 +0100

    add mappings to controllers

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,6 +3,7 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.donation.DonationRepository;
 import pl.coderslab.charity.institution.InstitutionRepository;
 
 
@@ -10,14 +11,19 @@ import pl.coderslab.charity.institution.InstitutionRepository;
 public class HomeController {
 
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @GetMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("quantityCount", donationRepository.quantityCount());
+        model.addAttribute("quantityAll", donationRepository.quantityAll());
         return "/index";
     }
+
 }
