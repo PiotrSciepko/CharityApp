35ddb0334ebb7637df10cfa5153cf2cb3cdccda4
PiotrSciepko <piotrek.priv@gmail.com>
2020-12-09 18:24:04 +0100

    add date/time format pattern to Donation class

--- src/main/java/pl/coderslab/charity/donation/Donation.java
+++ src/main/java/pl/coderslab/charity/donation/Donation.java
@@ -1,6 +1,7 @@
 package pl.coderslab.charity.donation;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 import pl.coderslab.charity.category.Category;
 import pl.coderslab.charity.institution.Institution;
 
@@ -32,6 +33,8 @@ public class Donation {
 
     @Column(columnDefinition = "char(6)")
     private String zipCode;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate pickUpDate;
     private LocalTime pickUpTime;
     private String pickUpComment;
