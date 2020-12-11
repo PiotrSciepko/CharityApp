f953bfdd2a55a948e5caf667d893e99387778c9d
PiotrSciepko <piotrek.priv@gmail.com>
2020-12-11 19:46:17 +0100

    add mappings to DonationController

--- src/main/java/pl/coderslab/charity/DonationController.java
+++ src/main/java/pl/coderslab/charity/DonationController.java
@@ -1,14 +1,52 @@
 package pl.coderslab.charity;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.coderslab.charity.category.Category;
+import pl.coderslab.charity.category.CategoryRepository;
+import pl.coderslab.charity.donation.Donation;
+import pl.coderslab.charity.donation.DonationRepository;
+import pl.coderslab.charity.institution.Institution;
+import pl.coderslab.charity.institution.InstitutionRepository;
+
+import java.util.Collection;
 
 @Controller
 public class DonationController {
 
+    private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
+
+    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository, DonationRepository donationRepository) {
+        this.categoryRepository = categoryRepository;
+        this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
+    }
+
     @GetMapping("/form")
-    public String formAction() {
+    public String formAction(Model model) {
+        model.addAttribute("donation", new Donation());
         return "/form";
+    }
+
+    @PostMapping("/form")
+    public String formAction(Donation donation) {
+        donationRepository.save(donation);
+        return "redirect:/";
+    }
+
+    @ModelAttribute("categoryList")
+    public Collection<Category> categoryList() {
+        return this.categoryRepository.findAll();
+    }
+
+    @ModelAttribute("institutions")
+    public Collection<Institution> institutions() {
+        return this.institutionRepository.findAll();
     }
 
 }
